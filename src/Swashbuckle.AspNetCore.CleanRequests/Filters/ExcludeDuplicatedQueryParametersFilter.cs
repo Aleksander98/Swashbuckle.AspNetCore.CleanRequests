23e6b94852d337747e1980c9166dd48705f2644d
Aleksander98 <aleksander.woznica98@gmail.com>
2022-07-31 09:56:28 +0200

    Add ExcludeDuplicatedQueryParametersFilter

---
+++ src/Swashbuckle.AspNetCore.CleanRequests/Filters/ExcludeDuplicatedQueryParametersFilter.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+
+namespace Swashbuckle.AspNetCore.CleanRequests.Filters
+{
+    /// <summary>
+    /// Operation filter to exclude query parameters that are already defined for e.g. in path.
+    /// </summary>
+    public class ExcludeDuplicatedQueryParametersFilter : IOperationFilter
+    {
+        private readonly ParameterLocation[] _parameterLocations;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="parameterLocations">Locations to search for duplicates.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public ExcludeDuplicatedQueryParametersFilter(params ParameterLocation[] parameterLocations)
+        {
+            _parameterLocations = parameterLocations ?? throw new ArgumentNullException(nameof(parameterLocations));
+
+            if (_parameterLocations.Contains(ParameterLocation.Query))
+            {
+                throw new ArgumentException("ParameterLocation.Query is not allowed in this context.", nameof(_parameterLocations));
+            }
+        }
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext _)
+        {
+            var pathParameters = operation.Parameters
+                .Where(parameter => _parameterLocations.Any(parameterLocation => parameter.In == parameterLocation))
+                .ToArray();
+
+            if (pathParameters.Length <= 0) return;
+
+            var queryParameters = operation.Parameters
+                .Where(parameter => parameter.In == ParameterLocation.Query)
+                .ToArray();
+
+            foreach (var queryParameter in queryParameters)
+            {
+                if (pathParameters.Any(pathParameter => pathParameter.Name.Equals(queryParameter.Name, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    operation.Parameters.Remove(queryParameter);
+                }
+            }
+        }
+    }
+}
---
+++ test/Swashbuckle.AspNetCore.CleanRequests.UnitTests/Filters/ExcludeDuplicatedQueryParametersFilterTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.CleanRequests.Filters;
+
+namespace Swashbuckle.AspNetCore.CleanRequests.UnitTests.Filters;
+
+public class ExcludeDuplicatedQueryParametersFilterTests
+{
+    [Fact]
+    public void ShouldThrowArgumentNullExceptionWhenParameterLocationsIsNull()
+    {
+        // Arrange, Act, Assert
+        Should.Throw<ArgumentNullException>(() => new ExcludeDuplicatedQueryParametersFilter(null));
+    }
+
+    [Fact]
+    public void ShouldNotThrowWhenParameterLocationsIsEmpty()
+    {
+        // Arrange, Act, Assert
+        Should.NotThrow(() => new ExcludeDuplicatedQueryParametersFilter());
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenParameterLocationsContainsParameterLocationQuery()
+    {
+        // Arrange, Act, Assert
+        Should.Throw<ArgumentException>(() => new ExcludeDuplicatedQueryParametersFilter(
+            ParameterLocation.Path,
+            ParameterLocation.Query,
+            ParameterLocation.Cookie, 
+            ParameterLocation.Header)
+        );
+    }
+
+    [Theory]
+    [InlineData(ParameterLocation.Header)]
+    [InlineData(ParameterLocation.Cookie)]
+    [InlineData(ParameterLocation.Path)]
+    public void ShouldExcludeDuplicatedParametersFromQuery(ParameterLocation parameterLocation)
+    {
+        // Arrange
+        const string duplicatedParameterName = "DuplicatedParameter";
+        var operation = new OpenApiOperation
+        {
+            Parameters = new List<OpenApiParameter>
+            {
+                new()
+                {
+                    In = ParameterLocation.Cookie,
+                    Name = "CookieParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Header,
+                    Name = "HeaderParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Path,
+                    Name = "PathParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Query,
+                    Name = "QueryParameter"
+                },
+                new()
+                {
+                    In  = parameterLocation,
+                    Name = duplicatedParameterName
+                },
+                new()
+                {
+                    In = ParameterLocation.Query,
+                    Name = duplicatedParameterName
+                }
+            }
+        };
+        var filter = new ExcludeDuplicatedQueryParametersFilter(parameterLocation);
+
+        // Act
+        filter.Apply(operation, null);
+
+        // Assert
+        operation.Parameters.Count.ShouldBe(5);
+        operation.Parameters.ShouldNotContain(parameter =>
+            parameter.Name == duplicatedParameterName &&
+            parameter.In == ParameterLocation.Query
+        );
+    }
+
+    [Theory]
+    [InlineData(ParameterLocation.Header)]
+    [InlineData(ParameterLocation.Cookie)]
+    [InlineData(ParameterLocation.Path)]
+    public void ShouldNotExcludeAnyParametersFromQueryWhenThereIsNoDuplicates(ParameterLocation parameterLocation)
+    {
+        // Arrange
+        var operation = new OpenApiOperation
+        {
+            Parameters = new List<OpenApiParameter>
+            {
+                new()
+                {
+                    In = ParameterLocation.Cookie,
+                    Name = "CookieParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Header,
+                    Name = "HeaderParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Path,
+                    Name = "PathParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Query,
+                    Name = "QueryParameter"
+                },
+                new()
+                {
+                    In = ParameterLocation.Query,
+                    Name = "OtherQueryParameter"
+                }
+            }
+        };
+        var filter = new ExcludeDuplicatedQueryParametersFilter(parameterLocation);
+
+        // Act
+        filter.Apply(operation, null);
+
+        // Assert
+        operation.Parameters.Count.ShouldBe(5);
+    }
+}
