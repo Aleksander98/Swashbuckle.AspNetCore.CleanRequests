43096458d723be594e6fe876ea72aa739e3b2e2f
Aleksander98 <aleksander.woznica98@gmail.com>
2022-07-31 13:32:36 +0200

    Add examples project

---
+++ src/Swashbuckle.AspNetCore.CleanRequests.Examples/Models/CreateOrderItemRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Swashbuckle.AspNetCore.CleanRequests.Examples.Models;
+
+public class CreateOrderItemRequest
+{
+    public int OrderId { get; set; }
+    
+    public string? Name { get; set; }
+
+    public bool Available { get; set; }
+}
